9865e27f2bc0747638753e4f710ee6a6750c55ce
Dmitry <dark.knight.9111@gmail.com>
2025-07-16 14:54:17 +0300

    chore: upgrade dependencies and refactor for Storybook 9 compatibility

--- src/withOverlay.ts
+++ src/withOverlay.ts
@@ -1,46 +1,50 @@
-import { useChannel, useEffect, useState, makeDecorator } from "storybook/preview-api";
-
+import {
+  useChannel,
+  useEffect,
+  useState,
+  makeDecorator,
+} from "storybook/preview-api";
 
 import { DEFAULT_DYNAMIC_OVERLAY_OPTIONS, EVENTS } from "./constants";
 import { DynamicOverlayOptions } from "./types";
-import { renderOverlay, removeOverlay } from './utils/overlay';
+import { renderOverlay, removeOverlay } from "./utils/overlay";
 
-export const withOverlay = makeDecorator(
-  {
-    name: 'withOverlay',
-    parameterName: 'pixelPerfect',
-    skipIfNoParametersOrOptions: false,
-    wrapper: (StoryFn, context) => {
-  const global = context.globals.pixelPerfect;
-  const parameter = context.parameters.pixelPerfect;
-  const [
-    currentDynamicOverlayOptions,
-    setCurrentDynamicOverlayOptions
-  ] = useState({});
+export const withOverlay = makeDecorator({
+  name: "withOverlay",
+  parameterName: "pixelPerfect",
+  skipIfNoParametersOrOptions: false,
+  wrapper: (StoryFn, context) => {
+    const global = context.globals.pixelPerfect;
+    const parameter = context.parameters.pixelPerfect;
+    const [currentDynamicOverlayOptions, setCurrentDynamicOverlayOptions] =
+      useState({});
 
-  useChannel({
-    [EVENTS.DYNAMIC_OVERLAY_OPTIONS_CHANGED]: (dynamicOverlayOptions: DynamicOverlayOptions) => {
-      setCurrentDynamicOverlayOptions(dynamicOverlayOptions);
-    },
-  });
+    useChannel({
+      [EVENTS.DYNAMIC_OVERLAY_OPTIONS_CHANGED]: (
+        dynamicOverlayOptions: DynamicOverlayOptions
+      ) => {
+        setCurrentDynamicOverlayOptions(dynamicOverlayOptions);
+      },
+    });
 
-  useEffect(() => {
-    if (global?.active && parameter) {
-      renderOverlay({
-        ...DEFAULT_DYNAMIC_OVERLAY_OPTIONS,
-        ...parameter.overlay,
-        ...currentDynamicOverlayOptions,
-      });
-    } else {
-      removeOverlay();
-    }
-  }, [global?.active, parameter, currentDynamicOverlayOptions]);
+    useEffect(() => {
+      if (global?.active && parameter) {
+        renderOverlay({
+          ...DEFAULT_DYNAMIC_OVERLAY_OPTIONS,
+          ...parameter.overlay,
+          ...currentDynamicOverlayOptions,
+        });
+      } else {
+        removeOverlay();
+      }
+    }, [global?.active, parameter, currentDynamicOverlayOptions]);
 
-  useEffect(() => {
-    return () => {
-      removeOverlay();
-    };
-  }, []);
+    useEffect(() => {
+      return () => {
+        removeOverlay();
+      };
+    }, []);
 
-  return StoryFn(context);
-}});
+    return StoryFn(context);
+  },
+});
